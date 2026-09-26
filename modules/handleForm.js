c2315e44023b8c7022dc6a59f7211dba4126c53f
João Antonio <contatojoao87@gmail.com>
2026-09-26 12:15:58 -0300

    feat: iniciar js form cadastro

---
+++ modules/handleForm.js
@@ -0,0 +1,11 @@
+export default function handleForm() {
+  const form = document.querySelector("#form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+
+    return data;
+  });
+}
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 import handleDarkMode from "./modules/handleDarkMode.js";
+import handleForm from "./modules/handleForm.js";
 
 handleDarkMode();
+handleForm();
